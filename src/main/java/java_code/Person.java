59986f74c23f50ccbce9201af5b28aed89251fd9
KarakasMustafa <m.isik0229@gmail.com>
2019-12-19 12:02:01 -0600

    Added firstName instance variable

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -3,10 +3,11 @@ package java_code;
 public class Person {
 
     private int age;
+    String firstName;
 
     public Person(int age){
         this.age = age;
-        System.out.println("");
+
     }
 
 }
