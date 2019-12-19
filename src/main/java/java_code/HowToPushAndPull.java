31a92ce95b6005dc9f64ba81f1cfd2ab72c0d848
KarakasMustafa <m.isik0229@gmail.com>
2019-12-19 10:29:22 -0600

    added red color

---
+++ src/main/java/java_code/HowToPushAndPull.java
@@ -0,0 +1,25 @@
+package java_code;
+
+public class HowToPushAndPull {
+    /*
+    git status:
+        to check what is new in your repository.
+
+    On branch master:
+        Your branch is up to date with 'origin/master'.
+
+    Changes to be committed:
+        (use "git reset HEAD <file>..." to unstage)
+
+            new file:   colors.txt
+
+    Changes not staged for commit:
+        (use "git add <file>... " to update what will be committed)
+        (use "git checkout ---<file>"  to discard changes in working directory)
+
+            modified:   colors.txt
+
+    git add colors.txt - to stage
+
+     */
+}
