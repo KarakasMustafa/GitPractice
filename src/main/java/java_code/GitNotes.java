0a79d93727adf960eeaa1aa722c7459ff3880f0f
KarakasMustafa <m.isik0229@gmail.com>
2019-12-19 11:35:00 -0600

    Added notes

--- src/main/java/java_code/GitNotes.java
+++ src/main/java/java_code/GitNotes.java
@@ -2,6 +2,31 @@ package java_code;
 
 public class GitNotes {
     /*
+    11/22/2019
+
+    git is a version control tool,
+    it keeps track of changes that were made,
+    git remembers who did changes, when and what kind of changes,
+    it helps to collaborate, so multiple developers can easily work together on the same project.
+    Also, git is a distributed version control system(DVCS).
+
+    Main version of the project will be in the cloud ( for example github)
+
+    github : is a google drive for git projects/repositories
+
+    for project we create a git repository.
+    We have local repository( on your computer)
+    and we have remote repository (on github)
+
+    git knows about changes in our projects
+    github works as a cloud storage for our projects.
+
+    The process of getting a copy of projects call cloning
+
+    git clone - stands for getting a copy of projects from remote
+
+     */
+    /*
     git status:
         to check what is new in your repository.
 
